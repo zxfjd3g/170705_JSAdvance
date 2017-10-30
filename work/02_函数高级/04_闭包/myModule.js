3fd9a5d68bd4e906cabe8e41891910fe4ba630bf
zxfjd3g <258147149@qq.com>
2017-10-30 11:59:30 +0800

    闭包

---
+++ work/02_函数高级/04_闭包/myModule.js
@@ -0,0 +1,20 @@
+function myModule() {
+  // 私有数据
+  var data = 'atguigu'
+  // 操作数据的行为
+  function doSomething() {
+    console.log('doSomething()', data)
+  }
+  function doOtherthing() {
+    console.log('doOtherthing()', data)
+  }
+  function xxx() {
+    console.log('xxx()', data)
+  }
+
+  // 向外暴露部分操作数据的行为
+  return {
+    doSomething,
+    doOtherthing
+  }
+}
